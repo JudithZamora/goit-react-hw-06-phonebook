58a2d5b6909d3e85ec4b42df4ef3bd2eda2c5f45
King Nodens <judith.zamora.go@gmail.com>
2023-09-21 21:12:12 -0600

    refactorizacion, reduce

---
+++ src/components/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+import { useSelector, useDispatch } from 'react-redux'; 
+import { addContact, deleteContact, setFilter } from './slices/contactsSlice'; 
+
+const App = () => {
+  const contacts = useSelector((state) => state.contacts.contacts);
+  const filter = useSelector((state) => state.contacts.filter);
+  const dispatch = useDispatch();
+
+  const handleAddContact = ({ name, number }) => {
+    const existingContact = contacts.find(
+      (contact) => contact.name.toLowerCase() === name.toLowerCase()
+    );
+  
+    if (existingContact) {
+      alert('Contact already exists!');
+      return;
+    }
+  
+    const newContact = {
+      id: nanoid(),
+      name: name,
+      number: number,
+    };
+    dispatch(addContact(newContact));
+  };
+
+  const handleDeleteContact = (id) => {
+    dispatch(deleteContact(id));
+  };
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={handleAddContact} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} setFilter={(value) => dispatch(setFilter(value))} />
+      <ContactList
+        contacts={contacts}
+        deleteContact={handleDeleteContact}
+      />
+    </div>
+  );
+};
+
+export default App;
